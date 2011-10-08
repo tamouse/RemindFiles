d1f7d8751423b72cbc2d68efa9fa5a7489896b5d
Tamara Temple <tamouse@gmail.com>
2011-10-08 02:32:08 -0500

    Initial commit
     Changes to be committed:
    
    	new file:   .gitignore
    	new file:   AU_Holidays.rem
    	new file:   Defs.rem
    	new file:   Earth_Seasons.rem
    	new file:   UK_Holidays.rem
    	new file:   au.rem
    	new file:   birthday.rem
    	new file:   ca.rem
    	new file:   cal2rem.pl
    	new file:   calendar.mydays
    	new file:   christian.rem
    	new file:   computer.rem
    	new file:   history.rem
    	new file:   holiday.rem
    	new file:   lotr.rem
    	new file:   music.rem
    	new file:   mydays.rem
    	new file:   newzealand.rem
    	new file:   remindrc
    	new file:   ushol.rem

---
+++ cal2rem.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl -wn
+#
+# cal2rem
+#
+# Author: Tamara Temple <tamara@tamaratemple.com>
+# Created: 2011/10/07
+# Copyright (c) 2011 Tamara Temple Web Development
+#
+
+use strict;
+
+my @months = qw{Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec};
+
+chomp();
+
+/^LANG/ && next;
+/^#/ && next;
+
+if (/^[0-9]/) # line begins with MM/DD
+    {
+	my ($ds, @t) = split();
+	my ($m, $d) = split(/\//,$ds);
+	my $mn = $months[$m-1];
+	printf("REM %s %s MSG %s\n",$mn,$d,join(" ",@t));
+    }
+elsif (/^Easter/)
+{
+    my ($ds, $t) = split(/\t/);
+    printf("REM %s MSG %s\n", $ds, $t);
+}
+elsif (/^[A-Za-z]/) # line begins with Mmm dd
+    {
+	my $mn;
+	my $dp;
+	my ($ds, $t) = split(/\t/);
+	if ($ds =~ m|/|) {
+	    ($mn,$dp) = split(/\//,$ds); 
+	} else {
+	    ($mn,$dp) = split(/\s+/,$ds);
+	}
+	printf("REM %s %s MSG %s\n", $mn, $dp, $t);
+    }
+elsif (/\/\*\s+(.*)\s+\*\//)
+    {
+	printf("\n# %s\n",$1);
+    }
+else
+    {
+	# do nothing
+    }
+
+	   
+		
+
