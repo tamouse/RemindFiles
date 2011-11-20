539f5b203bee54deb2e8d1ca75f76b7d3712d69e
Tamara Temple <tamouse@gmail.com>
2011-11-20 11:49:49 -0600

    sort files

--- cal2rem.pl
+++ cal2rem.pl
@@ -17,37 +17,37 @@ chomp();
 /^#/ && next;
 
 if (/^[0-9]/) # line begins with MM/DD
-    {
-	my ($ds, @t) = split();
-	my ($m, $d) = split(/\//,$ds);
-	my $mn = $months[$m-1];
-	printf("REM %s %s MSG %s\n",$mn,$d,join(" ",@t));
-    }
+{
+    my ($ds, @t) = split();
+    my ($m, $d) = split(/\//,$ds);
+    my $mn = $months[$m-1];
+    printf("REM %s %s MSG %s\n",$mn,$d,join(" ",@t));
+}
 elsif (/^Easter/)
 {
     my ($ds, $t) = split(/\t/);
     printf("REM %s MSG %s\n", $ds, $t);
 }
 elsif (/^[A-Za-z]/) # line begins with Mmm dd
-    {
-	my $mn;
-	my $dp;
-	my ($ds, $t) = split(/\t/);
-	if ($ds =~ m|/|) {
-	    ($mn,$dp) = split(/\//,$ds); 
-	} else {
-	    ($mn,$dp) = split(/\s+/,$ds);
-	}
-	printf("REM %s %s MSG %s\n", $mn, $dp, $t);
+{
+    my $mn;
+    my $dp;
+    my ($ds, $t) = split(/\t/);
+    if ($ds =~ m|/|) {
+	($mn,$dp) = split(/\//,$ds); 
+    } else {
+	($mn,$dp) = split(/\s+/,$ds);
     }
+    printf("REM %s %s MSG %s\n", $mn, $dp, $t);
+}
 elsif (/\/\*\s+(.*)\s+\*\//)
-    {
-	printf("\n# %s\n",$1);
-    }
+{
+    printf("\n# %s\n",$1);
+}
 else
-    {
-	# do nothing
-    }
+{
+    # do nothing
+}
 
 	   
 		
